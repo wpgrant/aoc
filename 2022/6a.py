796ab9f7b128cfb11e2d73cefa8a9bec2188197d
Bill Grant <wpgrant@gmail.com>
2022-12-17 16:25:31 -0500

    day6

---
+++ 2022/6a.py
@@ -0,0 +1,26 @@
+def findDupe(theString):
+  testString = theString[0:1]
+  for i in range(1, len(theString)):
+    theChar = theString[i:i+1]
+    if (testString.find(theChar) >= 0):
+      return True
+    else:
+      testString += theChar
+  
+  return False
+
+
+if __name__ == '__main__':
+  with open('6.txt', 'r') as file:
+    theSignal = file.readline()
+    i = 4
+    isFound = False
+    while(i < len(theSignal) + 1):
+      testChars = theSignal[i-4:i]
+      if not findDupe(testChars):
+        print(i)
+        quit()
+        
+      i += 1
+
+  print("Not found.")
