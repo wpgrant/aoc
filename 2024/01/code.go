23ce21655a73bf8a0f11a3f1f28327788a222b3a
Bill Grant <wpgrant@gmail.com>
2024-12-08 17:35:02 -0500

    day1

--- 2024/01/code.go
+++ 2024/01/code.go
@@ -1,68 +1,109 @@
 package main
 
 import (
-  "bufio"
-  "fmt"
-  "os"
-  "sort"
-  "strconv"
-  "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func fileToArray(aFile *os.File) []string {
-  fileScanner := bufio.NewScanner(aFile)
-  fileScanner.Split(bufio.ScanLines)
-  var fileLines []string
+	fileScanner := bufio.NewScanner(aFile)
+	fileScanner.Split(bufio.ScanLines)
+	var fileLines []string
 
-  for fileScanner.Scan() {
-    fileLines = append(fileLines, fileScanner.Text())
-  }
+	for fileScanner.Scan() {
+		fileLines = append(fileLines, fileScanner.Text())
+	}
 
-  aFile.Close()
+	aFile.Close()
 
-  return fileLines
+	return fileLines
 }
 
 func processFileA(fileLines []string) {
-  // Parse lines into 2 slices, sort them, and find distances and add
-  left := []int{}
-  right := []int{}
-  for _, line := range fileLines {
-    nums := strings.Split(line, "   ")
-    leftInt, err := strconv.Atoi(nums[0])
-    if err != nil {
-      fmt.Println(err)
-    }
-    rightInt, err := strconv.Atoi(nums[1])
-    if err != nil {
-      fmt.Println(err)
-    }
-    left = append(left, leftInt)
-    right = append(right, rightInt)
-  }
-  sort.Ints(left)
-  sort.Ints(right)
+	// Parse lines into 2 slices, sort them, and find distances and add
+	left, right := createLists(fileLines)
+	sort.Ints(left)
+	sort.Ints(right)
 
-  diffTotal := 0
-  for i:=0; i<len(left); i++ {
-    diff := abs(left[i] - right[i])
-    diffTotal += diff
-  }
-  fmt.Println(diffTotal)
+	diffTotal := 0
+	for i := 0; i < len(left); i++ {
+		diff := abs(left[i] - right[i])
+		diffTotal += diff
+	}
+	fmt.Println("a: ", diffTotal)
+}
+
+func processFileB(fileLines []string) {
+	// Parse lines into 2 slices, for each number on left count instances, multiply and add
+	left, right := createLists(fileLines)
+	mapOcc := map[int]int{}
+	total := 0
+	for i := 0; i < len(left); i++ {
+		if mapOcc[left[i]] == 0 {
+			newCnt := countVals(right, left[i])
+			mapOcc[left[i]] = newCnt
+			if newCnt >= 0 {
+				total += left[i] * newCnt
+			}
+		} else if mapOcc[left[i]] > 0 {
+			total += left[i] * mapOcc[left[i]]
+		}
+	}
+	fmt.Println("b: ", total)
+}
+
+func countVals(theSlice []int, theNum int) int {
+	cnt := 0
+	for i := 0; i < len(theSlice); i++ {
+		if theSlice[i] == theNum {
+			cnt += 1
+		}
+	}
+	if cnt > 0 {
+		return cnt
+	} else {
+		// Populate it with -1 so it's ignored later
+		return -1
+	}
+}
+
+func createLists(fileLines []string) ([]int, []int) {
+	// Parse lines into 2 slices, sort them, and find distances and add
+	left := []int{}
+	right := []int{}
+	for _, line := range fileLines {
+		nums := strings.Split(line, "   ")
+		leftInt, err := strconv.Atoi(nums[0])
+		if err != nil {
+			fmt.Println(err)
+		}
+		rightInt, err := strconv.Atoi(nums[1])
+		if err != nil {
+			fmt.Println(err)
+		}
+		left = append(left, leftInt)
+		right = append(right, rightInt)
+	}
+	return left, right
 }
 
 func abs[T float64 | int](x T) T {
-  if x < 0 {
-    return -x
-  }
-  return x
+	if x < 0 {
+		return -x
+	}
+	return x
 }
 
 func main() {
-  aFile, err := os.Open("01a.txt")
-  if err != nil {
-    fmt.Println(err)
-  }
-  aLines := fileToArray(aFile)
-  processFileA(aLines)
+	aFile, err := os.Open("01.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	aLines := fileToArray(aFile)
+	processFileA(aLines)
+	processFileB(aLines)
 }
